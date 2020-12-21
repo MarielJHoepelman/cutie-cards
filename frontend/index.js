9fd12afe6ed11d39e14337e33c62b23bc9497cc6
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-12-20 20:17:37 -0500

    Imnports and sets up initial instances of Registration and Game classes.

---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+import { Registration } from "./modules/registration.js";
+import { Game } from "./modules/game.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const registration = new Registration();
+  const game = new Game();
+});
