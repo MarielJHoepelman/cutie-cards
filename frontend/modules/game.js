d503872ea69c76c48e3d175acc1236127dcb3e21
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-01-05 13:19:17 -0500

    [Game] refactors displayCards function and creates winChecker function.
    - Compartmentalizes the functionality of displayCards into separate 
    functions.
    - Creates winChecker function and defines winning or losing conditional 
    statements.

--- frontend/modules/game.js
+++ frontend/modules/game.js
@@ -1,27 +1,35 @@
+import { Fetcher } from "./fetcher.js";
+
 export class Game {
   CARDS = [
-    "strawberry",
+    //"strawberry",
     "apple",
-    "cherry",
+    //"cherry",
     "coffee",
     "avocado",
-    "watermelon",
-    "kiwi",
-    "applejuice",
-    "bubbletea",
-    "donut",
-    "chocolatemilk",
-    "matchatea",
-    "strawberrymilk",
+    //"watermelon",
+    //"kiwi",
+    //"applejuice",
+    //"bubbletea",
+    //"donut",
+    //"chocolatemilk",
+    //"matchatea",
+    //"strawberrymilk",
     "popsie",
   ];
 
   INITIAL_TIMER = 5000;
   FLIP_TIMER = 500;
+  MOVES_LIMIT = 10;
+  MATCHES_TO_WIN = 4;
+
+  matchesCounter = 0; //adds one every time a match is made.
+  movesLeft = 0;
 
   clickedCardMemo = null;
 
   constructor() {
+    this.movesLeft = this.MOVES_LIMIT;
     this.displayCards();
     this.flipCards(this.INITIAL_TIMER);
   }
@@ -88,68 +96,109 @@ export class Game {
     return cardOne === cardTwo;
   };
 
+  winChecker = () => {
+    if (this.movesLeft === 0) {
+      //show loose banner
+      //play again yes no
+      console.log("booooo");
+    } else if (this.matchesCounter === this.MATCHES_TO_WIN) {
+      //show win banner
+      //play again yes no
+      console.log("yayyyyyy");
+    }
+  };
+
+  createCard = (element, imageTag) => {
+    let card = document.createElement("button");
+    card.classList.add("card-button");
+    card.setAttribute("data-id", element);
+    card.appendChild(imageTag);
+    return card;
+  };
+
+  createImageTag = (source, format) => {
+    let imageTag = document.createElement("img");
+    imageTag.src = `./images/${source}.${format}`;
+    return imageTag;
+  };
+
+  createflipContainer = () => {
+    let cardContainer = document.createElement("div");
+    cardContainer.classList.add("flip-container", "card");
+    return cardContainer;
+  };
+
+  createFlipperElement = () => {
+    let flipper = document.createElement("div");
+    flipper.classList.add("flipper");
+    return flipper;
+  };
+
+  createCardAngleElement = (angle) => {
+    let side = document.createElement("div");
+    side.classList.add(angle);
+    return side;
+  };
+
+  timeSetter = (parentElement) => {
+    let timeSetter = setTimeout((timer) => {
+      this.clickedCardMemo.closest(".flip-container").classList.add("flipped"); // fliped the first card
+
+      parentElement.classList.add("flipped"); // flip the second card
+
+      this.clickedCardMemo = null; // reset the memo card
+      this.enableClick();
+    }, this.FLIP_TIMER);
+    return timeSetter;
+  };
+
+  addListener = (cardButton) => {
+    cardButton.addEventListener("click", (event) => {
+      const currentClickedCard = event.target;
+      const parentDiv = currentClickedCard.closest(".flip-container"); // same as parent parent parent
+
+      parentDiv.classList.remove("flipped"); // flip this card to see it
+
+      if (!this.clickedCardMemo) {
+        // First click
+        this.clickedCardMemo = currentClickedCard; // this is the first card clicked, save it
+      } else {
+        // Second click
+        this.movesLeft -= 1;
+        this.disableClick();
+        const matched = this.matchTracker(
+          currentClickedCard.dataset.id,
+          this.clickedCardMemo.dataset.id
+        );
+
+        if (matched) {
+          /// score!
+          this.matchesCounter += 1;
+          this.clickedCardMemo = null; // reset the memo card
+          this.enableClick();
+        } else {
+          this.timeSetter(parentDiv);
+        }
+
+        this.winChecker();
+
+        console.log("matches", this.matchesCounter);
+        console.log(this.movesLeft);
+      }
+    });
+  };
+
   displayCards = () => {
     for (const element of this.shuffleCards(this.duplicateCards())) {
-      let flipContainer = document.createElement("div");
-      flipContainer.classList.add("flip-container");
-      flipContainer.classList.add("card");
+      let flipContainer = this.createflipContainer();
+      let flipper = this.createFlipperElement();
+      let front = this.createCardAngleElement("front");
+      let back = this.createCardAngleElement("back");
+      let frontCardImageTag = this.createImageTag(element, "jpg");
+      let reverseCardImageTag = this.createImageTag("reverse", "png");
+      let cardButton = this.createCard(element, reverseCardImageTag);
 
-      let flipper = document.createElement("div");
-      flipper.classList.add("flipper");
-
-      let front = document.createElement("div");
-      front.classList.add("front");
-
-      let back = document.createElement("div");
-      back.classList.add("back");
-
-      let frontCardImageTag = document.createElement("img");
-      frontCardImageTag.src = `./images/${element}.jpg`;
-
-      let reverseCardImageTag = document.createElement("img");
-      reverseCardImageTag.src = "./images/reverse.png";
-
-      let cardButton = document.createElement("button");
-      cardButton.classList.add("card-button");
-      cardButton.setAttribute("data-id", element);
-      cardButton.appendChild(reverseCardImageTag);
-
-      cardButton.addEventListener("click", (event) => {
-        const currentClickedCard = event.target;
-        const parentDiv = currentClickedCard.closest(".flip-container"); // same as parent parent parent
-
-        parentDiv.classList.remove("flipped"); // flip this card to see it
-
-        if (!this.clickedCardMemo) {
-          // First click
-          this.clickedCardMemo = currentClickedCard; // this is the first card clicked, save it
-        } else {
-          // Second click
-
-          this.disableClick();
-          const matched = this.matchTracker(
-            currentClickedCard.dataset.id,
-            this.clickedCardMemo.dataset.id
-          );
-
-          if (matched) {
-            /// score!
-            this.clickedCardMemo = null; // reset the memo card
-            this.enableClick();
-          } else {
-            setTimeout((timer) => {
-              this.clickedCardMemo
-                .closest(".flip-container")
-                .classList.add("flipped"); // fliped the first card
-
-              parentDiv.classList.add("flipped"); // flip the second card
-
-              this.clickedCardMemo = null; // reset the memo card
-              this.enableClick();
-            }, this.FLIP_TIMER);
-          }
-        }
-      });
+      this.addListener(cardButton);
 
       front.appendChild(frontCardImageTag);
       back.appendChild(cardButton);
