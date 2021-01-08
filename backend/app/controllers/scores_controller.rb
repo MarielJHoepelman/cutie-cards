520e46505bd4c0ba712fa7fcfae768b7e122996c
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-01-07 19:32:20 -0500

    [Controllers] Refactors Scores Controller.
    - Disables forgery protection on the controller by skipping the 
    verification before action.
    - Creates a strong params method that whitelists the parameters that are 
    permitted to be sent to the database.
    - Adds create function that persists a score in the database when a game 
    round is won and renders a JSON to the server.

--- backend/app/controllers/scores_controller.rb
+++ backend/app/controllers/scores_controller.rb
@@ -1,2 +1,12 @@
 class ScoresController < ApplicationController
+skip_before_action :verify_authenticity_token
+
+  def create
+    data = Score.create(score_params)
+    render json: {user_id: data.user_id, score: data.score}
+  end
+
+  def score_params
+    params.require(:data).permit(:user_id, :score)
+  end
 end
