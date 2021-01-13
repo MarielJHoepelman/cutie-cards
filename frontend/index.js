3c0405a9fb7fbb606f8bb979782b16f61ae16756
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-01-13 11:11:12 -0500

    [Index] Refactors index.js.
    - Removes the Game import statement at the top of the file.
    - Removes instantiation of Game from listener.

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,5 @@
 import { Registration } from "./modules/registration.js";
-import { Game } from "./modules/game.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const registration = new Registration();
-  const game = new Game();
 });
