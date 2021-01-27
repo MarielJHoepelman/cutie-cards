b300ae087d572f6bc017bfcb584e5f1905d617c2
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-01-27 16:06:34 -0500

    [Backend] Removes unused Cards model/controller and drops Cards table.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_200559) do
+ActiveRecord::Schema.define(version: 2021_01_27_205020) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
