c28878146cec4f9d8b7aa3d9c0dbe8daf2441cf4
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-05 16:25:46 -0500

    [Controllers] Adds functionality to Application.
    - Creates a helper method record_score that returns the best of all 
    scores. 
    - Renders value of record_score in Users controller create action.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :record_score
+
+  def record_score
+    Score.all.minimum(:score)
+  end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def create
     user = User.find_or_create_by(user_params)
     if user.valid?
-      render json: {name: user.name, id: user.id, best_score: user.best_score}
+      render json: {name: user.name, id: user.id, best_score: user.best_score, record: record_score}
     else
       render json: user.errors.messages.to_json, status: 500
     end
