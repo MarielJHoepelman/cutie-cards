b4bc7ba434a86554b98042a6f8650f9e0ab72c16
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-09 22:45:31 -0500

    [Seed] Populates the User database with initial data.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(:name => 'SalmaLovesOli')
+User.create(:name => 'DjEliel')
+User.create(:name => 'MiWillis')
+User.create(:name => 'Joseidy')
+User.create(:name => 'Alexandria')
+User.create(:name => 'AOC')
+User.create(:name => 'Selena')
+User.create(:name => 'KamalaVP')
+User.create(:name => 'BerniesMittens')
