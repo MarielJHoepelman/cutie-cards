e65212c70f319c87f9fe59fba7a624a63807dd4a
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-09 22:18:13 -0500

    [Models] Changes regex in Users model name validation to accept only characters and numbers instead of testing the presence of unwanted characters.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :scores
-  validates :name, presence: true, format: { with: /\A(?=.{3,12}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])\z/, message: "Oops! invalid format. Please enter a username between 3 and 8 characters long with no characters."}
+  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Oops! invalid format. Please enter a username between 3 and 8 characters long with no characters."}
 
   def best_score
     scores.minimum("score")
