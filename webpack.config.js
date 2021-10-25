3230556a1253b91af98e45c9b524d971ced91f96
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-10-25 18:46:49 -0400

    Installs file-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,14 @@ module.exports = {
           "css-loader",
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
 };
